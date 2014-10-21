5357122be3e5d0d870125a7832a70f0f3e5e1013
tdouglas <tdoug19@gmail.com>
2014-10-21 16:15:48 -0600

    Created a Merge Demo. I will use this to demonstrate branching and merging.

---
+++ MergeDemo/src/edu/ense374/MergeDemo/Merging.java
@@ -0,0 +1,26 @@
+/**
+ * Merging.java version 1.0  Oct 21, 2014
+ *
+ * Trevor Douglas
+ * 
+ */
+ 
+package edu.ense374.MergeDemo;
+
+/**
+ * @author tdouglas
+ *
+ */
+public class Merging {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	        System.out.println("Hello World!"); // Display the string.
+    }
+
+
+
+}
